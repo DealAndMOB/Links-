526d53e250857ed47a45c0c65747a97868ede60c
Dilan Raúl Martínez Hernández <dilan.martinez@ubimia.com>
2026-05-05 18:20:22 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// script.js
+
+const fadeElements = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver((entries) => {
+
+  entries.forEach(entry => {
+
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }
+
+  });
+
+}, {
+  threshold:0.15
+});
+
+fadeElements.forEach(el => {
+  observer.observe(el);
+});
